a5fca1cd0a2d8497a96254c5a1d8d41954f604ad
GoncaloG99 <guimaraes1920@hotmail.com>
2020-10-25 22:14:21 +0000

    Criação do Menu

--- app/src/main/java/ipvc/estg/projetopmeu_gg/MainActivity.kt
+++ app/src/main/java/ipvc/estg/projetopmeu_gg/MainActivity.kt
@@ -2,10 +2,31 @@ package ipvc.estg.projetopmeu_gg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId)
+        {
+            R.id.add -> true
+            R.id.remove -> true
+            R.id.edit -> true
+            R.id.selectMultiple -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
